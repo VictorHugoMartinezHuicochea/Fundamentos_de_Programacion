6ba6f46e47ee63e2dda9cb338e0f696a71c42f4a
Victor Hugo Martinez Huicochea <145628684+VictorHugoMartinezHuicochea@users.noreply.github.com>
2025-02-05 02:39:22 -0600

    Update TorresHanoi.java

--- TorresHanoi.java
+++ TorresHanoi.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Resuelva las Torres de Hanoi sin usar Recursividad
  */
 package torreshanoi;
 
